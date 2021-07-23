efa8693a715a8a0879009c6ffa3a920cd3956dde
Yulia <iuliiabarbash@gmail.com>
2021-07-23 12:34:49 -0400

    new project created

---
+++ 1stTest.test.ts
@@ -0,0 +1,6 @@
+
+const chromedriver = require("chromedriver");
+import { WebDriver, Builder, Capabilities } from "selenium-webdriver";
+const driver: WebDriver = new Builder()
+  .withCapabilities(Capabilities.chrome())
+  .build();
